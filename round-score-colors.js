24bffceb8ec872613efeaa218b451b8de8b22aa2
d-vorobiev <dmitry.s.vorobiev@gmail.com>
2017-02-20 20:08:45 +0300

    Basic implementation of simple doughnut chart

---
+++ round-score-colors.js
@@ -0,0 +1,22 @@
+var round_score_colors = [
+    "#c9b5bb", // Pale Silver
+    "#320a28", // Dark Purple
+    "#8e443d", // Chestnut
+    "#cb9173", // Antique Brass
+    "#511730", // Acajou
+    "#ffc09f", // Peach
+    "#ffee93", // Flavescent
+    "#a0ced9", // Pomp And Power
+    "#8d6b94", // Pale Cerulean
+    "#fcf5c7", // Lemon Chiffon
+    "#4b4b50", // Azureish White
+    "#454b5e", // Cadet Grey
+    "#727ca0", // Rhythm
+    "#9fa4b7", // Outer Space
+    "#d8ddef", // Dark Liver
+    "#52cc27",
+    "#8c9987",
+    "#f9ff7c",
+    "#ccbbff",
+    "#4f51cc"
+];
---
+++ round-score.js
@@ -0,0 +1,91 @@
+function DataLine(amount, label) {
+    this.amount = amount;
+    this.label = label;
+    this.percent = 0;
+}
+function RoundScore(svg, x, y, radius) {
+    this.draw = document.getElementById(svg);
+    this.x = x; //150;
+    this.y = y; //150;
+    this.radius = radius; //100;
+    this.data = [];
+    this.colors = round_score_colors;
+    this.add = function (amount, label) {
+        this.data[this.data.length] = new DataLine(amount, label);
+        return this;
+    };
+    this.load = function (arr) {
+        for (var i = 0; i < arr.length; i++) {
+            var line = arr[i];
+            this.add(line.amount, line.label);
+        }
+        return this;
+    };
+    this.recalc = function () {
+        var total = 0;
+        this.data.forEach(function (item) {
+            total += item.amount;
+        });
+        this.data.forEach(function (item) {
+            item.percent = Math.round(100 * item.amount / total);
+        });
+        this.data.sort(function (a, b) {
+            return b.percent - a.percent;
+        });
+		var checkPercentage = 0;
+		this.data.forEach(function (item) {
+			checkPercentage += item.percent;
+		});
+		if (checkPercentage > 100) {
+			this.data[this.data.length - 1].percent -= checkPercentage - 100;
+		}
+    };
+    this.show = function () {
+        this.recalc();
+        var em = 20;
+        var offset = this.y - this.data.length * em / 2;
+        var cursor = 0;
+        for (var i = 0; i < this.data.length; i++) {
+            var line = this.data[i];
+            var color = this.colors[i];
+            var nextPos = cursor + line.percent;
+            var startAngle = Math.round(cursor * 3.6);
+            var endAngle = Math.round(nextPos * 3.6);
+            var arc = asSvgPathArc(this.x, this.y, this.radius, startAngle, endAngle);
+
+            var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+            path.setAttribute("id", "path" + i);
+            path.setAttribute("fill", "none");
+            path.setAttribute("stroke", color);
+            path.setAttribute("stroke-width", "20");
+            path.setAttribute("d", arc);
+            this.draw.appendChild(path);
+
+            var xr = 1.75 * this.x + 1.5 * em;
+            var yr = offset + i * em;
+
+            var xt = xr + em;
+            var yt = yr + (0.6 * em);
+
+            var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+            rect.setAttribute("id", "rect" + i);
+            rect.setAttribute("x", xr.toString());
+            rect.setAttribute("y", yr.toString());
+            rect.setAttribute("width", "15");
+            rect.setAttribute("height", "15");
+            rect.setAttribute("fill", color);
+            this.draw.appendChild(rect);
+
+            var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+            text.setAttribute("id", "text" + i);
+            text.setAttribute("x", xt.toString());
+            text.setAttribute("y", yt.toString());
+            var s = line.label + " (" + line.amount + ", " + line.percent + "%)";
+            var t = document.createTextNode(s);
+            text.appendChild(t);
+            this.draw.appendChild(text);
+
+            cursor = nextPos;
+        }
+    }
+}
